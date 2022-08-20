bad698936e37101e9865a65ed0b9062d45697449
kurotom <55354389+kurotom@users.noreply.github.com>
2022-08-20 19:02:24 -0400

    corregido Tic_tac_toe

--- Tic_Tac_Toe/Tic_Tac_Toe.py
+++ Tic_Tac_Toe/Tic_Tac_Toe.py
@@ -9,20 +9,20 @@ def stringify():
     return string_gify
 
 
-def gana_o_no(itm_X_or_O, lista_score):    
+def gana_o_no(itm_X_or_O, lista_score):
     a = [dictado_listas["l1"][0], dictado_listas["l1"][1], dictado_listas["l1"][2]].count(itm_X_or_O)
     b = [dictado_listas["l2"][0], dictado_listas["l2"][1], dictado_listas["l2"][2]].count(itm_X_or_O)
     c = [dictado_listas["l3"][0], dictado_listas["l3"][1], dictado_listas["l3"][2]].count(itm_X_or_O)
-    
+
     d = [dictado_listas["l1"][0], dictado_listas["l2"][0], dictado_listas["l3"][0]].count(itm_X_or_O)
     e = [dictado_listas["l1"][1], dictado_listas["l2"][1], dictado_listas["l3"][1]].count(itm_X_or_O)
     f = [dictado_listas["l1"][2], dictado_listas["l2"][2], dictado_listas["l3"][2]].count(itm_X_or_O)
-    
+
     g = [dictado_listas["l1"][0], dictado_listas["l2"][1], dictado_listas["l3"][2]].count(itm_X_or_O)
     h = [dictado_listas["l3"][0], dictado_listas["l2"][1], dictado_listas["l1"][2]].count(itm_X_or_O)
     res = a,b,c,d,e,f,g,h
     lista_score.clear()
-    lista_score.append(res)   
+    lista_score.append(res)
     return [a,b,c,d,e,f,g,h], itm_X_or_O
 
 
@@ -50,8 +50,11 @@ turno = 0
 itm_win = ""
 quebrar = True
 
+
+
+print(stringify())
 while True:
-    
+
     if quebrar is False:
         print("\n")
         print(f"¡¡¡ Gana >>> {itm_win} <<< !!!")
@@ -64,12 +67,12 @@ while True:
         score.clear()
         dictado_listas = {"l1": [" ", " ", " "], "l2": [" ", " ", " "], "l3": [" ", " ", " "]}
         break
-    
+
     if turno % 2 == 0:
         print("Turno de:     O")
     elif turno % 2 == 1:
         print("Turno de:     X")
-        
+
     coordenadas = input("Coordenadas:").split()
     if len(coordenadas) > 0 and len(coordenadas) <= 2 and len(coordenadas) != 1:
         for item_lista in dictado_listas:
@@ -86,7 +89,7 @@ while True:
                 else:
                     print(obtenido[0])
                     print("\n")
-                    continue    
+                    continue
     else:
         print(stringify())
         continue
