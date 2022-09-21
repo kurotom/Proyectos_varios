b14d3742167500521c65f22a5ed3e5bd6ade62c0
kurotom <55354389+kurotom@users.noreply.github.com>
2022-09-21 11:35:47 -0300

    paquetes actualizadoo, cambio a class

--- scraping_Sismologia_cl/README.md
+++ scraping_Sismologia_cl/README.md
@@ -1,2 +1,24 @@
 # sismologia_scraping
-Obtiene los datos de sismologia y los guarda en json.
+Datos obtenidos de [http://www.sismologia.cl/](http://www.sismologia.cl/), se puede guardar los datos en un ficheros `.csv`.
+
+Utiliza driver [geckodriver - 0.31.0](https://github.com/mozilla/geckodriver/releases).
+
+
+## Requisitos
+* selenium==4.1.3
+* beautifulsoup4==4.11.0
+
+
+## ¿Cómo utilizar?
+
+```
+$ pip install -r requirements.txt
+```
+
+```
+$ python sismologia_scraping.py
+```
+
+
+Los datos se guardarán en un fichero '.csv' en el formato: `sismologia-[timestamp].csv`.
+Dicho 'timestamp' es del momento de la realización de la consulta de los datos.
--- scraping_Sismologia_cl/sismologia_scrapy.py
+++ scraping_Sismologia_cl/sismologia_scrapy.py
@@ -1,46 +1,107 @@
 # -*- coding: utf-8 -*-
 from time import sleep
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.firefox.service import Service
 from selenium.webdriver.firefox.options import Options
+
+from bs4 import BeautifulSoup
+from uuid import uuid4
+import datetime
 import json
-
-url = "http://www.sismologia.cl"
-
-opciones = Options()
-opciones.headless = True
-
-driver = webdriver.Firefox(options=opciones, executable_path="geckodriver/geckodriver")
-driver.get(url)
-
-sleep(3)
-
-frameset = driver.find_element_by_xpath("/html/frameset/frame[2]")
-driver.switch_to.frame(frameset)
-fr2 = driver.find_element_by_xpath("/html/frameset/frame[2]")
-driver.switch_to.frame(fr2)
-
-fr3 = driver.find_element_by_xpath("/html/body/div/div/div[2]/iframe")
-driver.switch_to.frame(fr3)
-
-tabla_datos = driver.find_elements_by_xpath("/html/body/table/tbody")
-
-lista = []
-for x in tabla_datos:
-    lista = x.text.split("\n")
-
-driver.close()
+import os
+import csv
 
 
-fin = []
-i = 0
-for x in lista:
-    if len(x.split()) > 4:
-        fecha = x.split()[:2]
-        ubicacion = x.split()[2:len(x.split()) - 2]
-        magnitud = x.split()[len(x.split()) - 2:]
-        res = {"fecha": " ".join(fecha), "ubicacion": " ".join(ubicacion), "magnitud": " ".join(magnitud)}
-        fin.append(res)
-    i += 1
+class SismologiaData(object):
+    def __init__(self, url, path):
+        self.url = url
+        self.agente = path
+        self.dataDict = {}
+        self.resultado = []
+        self.date = 0
 
-with open("temblores.json", "w") as file:
-    json.dump(fin, file)
+    def run(self):
+        self.date = int(datetime.datetime.now().timestamp())
+
+        opciones = Options()
+        opciones.headless = True
+
+        serviceObject = Service(self.agente)
+
+        driver = webdriver.Firefox(options=opciones, service=serviceObject)
+        driver.get(self.url)
+
+        sleep(3)
+
+        page = driver.page_source
+
+        s = BeautifulSoup(page, 'html.parser')
+
+        t = s.find('table', 'sismologia')
+        tr = t.find_all('tr')
+
+        for item in tr:
+            td = item.find_all('td')
+            if len(td) > 0:
+                id = str(uuid4())
+                linkMapa = self.url + td[0].find('a').get('href')
+                self.dataDict.update({id: [linkMapa]})
+
+        for element in self.dataDict:
+            info = self.dataDict[element].pop()
+
+            driver.get(info)
+
+            sleep(3)
+
+            s = BeautifulSoup(driver.page_source, 'html.parser')
+            tabla = s.find('table', "sismologia informe")
+            trs = tabla.find_all('tr')
+
+            infoDetail = []
+            for l in range(len(trs)):
+                if l == 1 or l == 2:
+                    td = trs[l].find_all('td')
+                    fechaHora = td[1].text.split()
+                    for dItem in fechaHora:
+                        infoDetail.append(dItem)
+                else:
+                    td = trs[l].find_all('td')
+                    infoDetail.append(td[1].text.strip())
+
+            urlIMG = info[:-4] + 'jpeg'
+            infoDetail.append(urlIMG)
+
+            self.resultado.append(infoDetail)
+
+        driver.close()
+
+    def write(self):
+        keysData = [
+            'Referencia',
+            'Hora local',
+            'Fecha local',
+            'Hora UTC',
+            'Fecha UTC',
+            'Latitud',
+            'Longitud',
+            'Profundidad',
+            'Magnitud',
+            'Mapa'
+        ]
+        with open(f'sismologia-{self.date}.csv', 'w') as csvfile:
+            writer = csv.writer(csvfile)
+            writer.writerow(keysData)
+            for line in self.resultado:
+                writer.writerow(line)
+
+
+
+
+if __name__ == '__main__':
+    url = "http://www.sismologia.cl"
+    path = os.path.abspath("geckodriver")
+    s = SismologiaData(url, path)
+    s.run()
+    s.write()
