9cdf407fbdbfa92c51bb65518656a13ba5a67d6e
kurotom <55354389+kurotom@users.noreply.github.com>
2023-03-11 19:46:02 -0300

    agregado reset_metadata_pdf

---
+++ change_meta_PDF/reset_metadata_pdf.sh
@@ -0,0 +1,99 @@
+#!/bin/bash
+# kurotom
+
+usage() {
+	echo -e "\n\t$0 -f pdf_original.pdf [ t | a | s | k | c | p ]\n"
+	echo -e """\tOptions:\n
+	\t-t title
+	\t-a author
+	\t-s subject
+	\t-k keywords
+	\t-c creator
+	\t-p producer
+	"""
+}
+
+
+runProgram() {
+	now=$(date +%Y%m%d%H%M%S)
+	original=$(echo $file)
+	final=$(echo "final-$file")
+
+	echo -e "\n$original --> $final"
+
+	marcas="""
+ 			[ /Title ($title)
+  		/Author ($author)
+	    /Subject ($subject)
+	    /Keywords ($keywords)
+ 	    /ModDate (D:$now)
+	    /CreationDate (D:$now)
+			/Creator ($creator)
+			/Producer ($producer)
+	    /DOCINFO pdfmark
+	"""
+
+	linkFile=$(readlink -f "$file")
+
+	if [[ -f $linkFile ]]; then
+
+		gs -q -dSAFER -dBATCH -dNOPAUSE -sDEVICE=pdfwrite -sOutputFile="$final" "$original" -c $marcas
+
+	else
+		echo -e "\a\nFichero origen no existe.\n"
+	fi
+
+	echo -e "\a\nFinalizado.\n"
+}
+
+
+
+
+file=""
+title=""
+author=""
+subject=""
+keywords=""
+creator=""
+producer=""
+
+
+while getopts :f:t:a:s:k:c:p: opcion ; do
+	case $opcion in
+		f)
+			file="${OPTARG}"
+			;;
+		t)
+			title=${OPTARG}
+			;;
+		a)
+			author=${OPTARG}
+			;;
+		s)
+			subject=${OPTARG}
+			;;
+		k)
+			keywords=${OPTARG}
+			;;
+		c)
+			creator=${OPTARG}
+			;;
+		p)
+			producer=${OPTARG}
+			;;
+		*)
+			usage
+			;;
+	esac
+done
+
+
+if [[ ${file} != "" ]]; then
+	runProgram
+else
+	usage
+fi
+
+
+
+# kurotom
